9684641a55815d1ba1e796b57f77b94c240e2022
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2023-10-17 12:22:58 -0700

    bootstrap next app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="grid place-items-center h-screen">
+      <div className="font-bold text-5xl">
+        Updates coming soon...
+      </div>
+    </main>
+  )
+}
